d47f08203c348808a8d3a280e8371729ac90fafa
= <=>
2015-10-02 20:06:27 -0500

    update gulp clean task

--- gulp_modules/clean.js
+++ gulp_modules/clean.js
@@ -1,8 +1,6 @@
 var del = require('del');
 module.exports = function(gulp) {
   gulp.task('clean', function() {
-    del(['./build/**/*']).then(function(){
-      console.log('build directory deleted');
-    });
+    del(['./build/**/*']);
   });
 }
